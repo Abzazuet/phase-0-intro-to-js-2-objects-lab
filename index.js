fed2496a1b056680de47ac9271335961245008c3
Abel Zazueta <abel.zazueta@gmail.com>
2022-03-16 13:48:41 -0700

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Abel",
+    streetAddress: "1161 Surf Crest Dr",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee
+}
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj
+}
